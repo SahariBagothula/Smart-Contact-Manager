b0df961985e507d1106dde4c79d52245318882b8
Sahari Bagothula <sahari.14bagothula@gmail.com>
2025-04-16 11:07:11 +0530

    Made some changes

--- src/main/java/com/scm/controllers/HomeController.java
+++ src/main/java/com/scm/controllers/HomeController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.scm.entities.User;
 import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -63,7 +67,7 @@ public class HomeController {
 
     //registering user
     @PostMapping("/do-register")
-    public String registerUser(@ModelAttribute UserForm userForm){
+    public String registerUser(@ModelAttribute UserForm userForm, HttpSession session){
         System.out.println("redirecting");
 
         //fetch data from form
@@ -73,15 +77,29 @@ public class HomeController {
         // save the data to database
 
         // converted data from userForn -> user
-        User user = User.builder()
-        .name(userForm.getName())
-        .email(userForm.getEmail())
-        .phoneNumber(userForm.getPhoneNumber())
-        .about(userForm.getAbout())
-        .password(userForm.getPassword())
-        .build();
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .about(userForm.getAbout())
+        // .password(userForm.getPassword())
+        // .build();
+
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setAbout(userForm.getAbout());
+        user.setPassword(userForm.getPassword());
 
         userService.saveUser(user);
+
+        Message message = Message.builder()
+                                 .content("Registration Successful")
+                                 .type(MessageType.green)
+                                 .build();
+
+        session.setAttribute("message", message);
         // message = "Registration Successful"
         // redirect to login page
 
